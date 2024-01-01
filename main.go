5aba3de7dda767be54c0c9135590d084fae6130a
Dviih <dviih2s@gmail.com>
2024-01-01 12:35:23 -0300

    In `init` get the flags and set to the variables.

--- main.go
+++ main.go
@@ -20,3 +20,9 @@ var (
 	quic    = os.Getenv("QUIC") != ""
 )
 
+func init() {
+	flag.StringVar(&addr, "address", ":80", "The address to listen to")
+	flag.StringVar(&tlsCert, "cert", "", "Path to the tls certificate")
+	flag.StringVar(&tlsKey, "key", "", "Path to the tls key")
+	flag.BoolVar(&quic, "quic", true, "Must have `tlsCert` and `tlsKey` to enable QUIC")
+
